40d9de21e95c8ea6c1d35355748758e24764eb21
MesiterWebzr <meister@fullsail.edu>
2014-01-16 15:01:52 -0800

    Expresion Industry
    setting up directory and declaring the given vars and removed personal
    expressions details

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,28 +1,11 @@
-//Otto Burroughs 1/16/2014 Expressions_Personal
+//Otto Burroughs 1/16/2014 Expressions_Industry
 
 
 
+//Industry web work calculator
 
 
-//Calculate your Quetelet Index or (BMI)
 
-
-var height = prompt("Enter your height in feet");//declaring the height value
-var inches = prompt("Enter your remaining height in inches");//declaring the inches value
-var mass = prompt("Enter your weight");//decelaring the mass (lbs) value
-var length = 12;//declaring the value of length for inches to be added
-var totalHeight = height * length + inches;//assigning level one calculation to declare total height
-var bmiPercentage = mass / totalHeight;//assigning level 2 calculation to set Quetelet Index or BMI
-var results = Math.round(bmiPercentage * 100);//calculting 
-var details = new Array( height + " " + "inches tall");
-
-/*alert("Your Quetelet Index is" + " " + results + "%" );//alerting user of results per  details inputed */
-
-alert("Based on your input of" +" " + height + "ft" + " " + inches +"in" + " " + "with body mass of" + " " + mass + "lbs" + " " + "Your Quetelet Index is" + " " + results + "%");//alerted details of user input
-
-console.log(height); //logged height into console
-console.log(inches);//logged inches into console
-console.log(mass);//logged mass into console
-console.log(totalHeight);//logged totalHeight into console
-console.log(bmiPercentage);//logged bmiPercentage into console
-console.log(results);//logged results into console
+var pages = prompt("Enter the amount of pages your website needs");
+var images = prompt("Enter the amount of images you need uploaded or edited");
+var links = prompt("How ")
