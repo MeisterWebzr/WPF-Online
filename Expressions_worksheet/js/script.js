56c88e3e908aa93893e1fd282464fa6c2d4bc25b
MesiterWebzr <meister@fullsail.edu>
2014-01-16 10:58:19 -0800

    Exspresions_Personal
    setup folder and directory for assignment

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,17 +2,47 @@
 
 
 
-
+//Dog Years
 //Calculate how old Sparky is by his actual age
 
+/* <---- remove to active calculator 
 
 
-
-
-var sparky = prompt("Enter age of dog in human years") //declared sparky and prompt for how old dog is in human years
+var sparky = prompt("Enter age of dog in human years"); //declared sparky and prompt for how old dog is in human years
 var sparkyAge = sparky * 7; //decalred value to dog aging vs human years
 var dogYears = "Sparky is " + " " + sparkyAge + " " + "dog" + " " + "years old"; //assigned the calculation to the resulting variable
-alert(dogYears) //alerted user with the age of dog in doggie years
+alert(dogYears); //alerted user with the age of dog in doggie years
+
+
 console.log(sparkyAge);//logged sparkyAge in console
+console.log(sparky);
+console.log(dogYears);
+
+//End Dog Years
+remove to active calculator ----> */ 
 
 
+//Slice of pie part 1
+//Calculate how much pizza everyone wil get at a party
+
+/* <---- remove to active calculator 
+
+
+
+var people = prompt("Enter the amount of people at the party"," " ); //Setting decleration of how many people at the party.
+var pizza = prompt("Enter the number of pizza for the party"," " ); //Setting value of pizza's to be ordered.
+var pizzaSlices = 10; //Set the value of slices per pizza.
+var perPerson = pizza * 10 / people; //Assigned the calculation to the resulting variable.
+
+
+
+alert("Each person ate" + " " + perPerson + " " + "slices" + " " + " of pizza at the party") //pop up notification with slices of pizza ate at party
+
+
+console.log(people); //logged number of people in console
+console.log(pizza); //logged numner of pizza's ordered in console
+console.log(pizzaSlices); //logged pizza slices per pizza in console
+console.log(perPerson); //logged number of slices each person ate at party in console
+
+
+//End Slice of Pie 1 remove to active calculator ----> */ 
