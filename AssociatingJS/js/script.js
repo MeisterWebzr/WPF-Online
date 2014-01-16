64ab845696752776bcb4bcc16c238b25e0845f08
MesiterWebzr <meister@fullsail.edu>
2014-01-15 16:25:38 -0800

    Update file from express
    editing file from last repo, moved into new repo to follow commits for
    Array listing

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,57 @@
-alert("This is in the js file");
+//JavaScript
+
+/*
+var yearBorn = 1955;
+console.log("Hello World");
+*/
+
+
+
+//Declaring a variable
+
+/*
+var age; //declaring a variable
+age = 32; //defining a variable
+//storage containre = value
+
+var yearOfBirth = 1970; // declaring and defining on the same line
+
+*/
+
+//End Declaring of Vairables
+
+
+
+//String and Boolean Variables
+
+
+/*
+
+var age = 32; //Number variable
+
+var isStudent = true; //Boolean variable
+
+var kermit = "my name";
+var firstName = "Kermit";
+
+
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
+
+
+*/
+
+//End Boolean Vairables//
+
+
+//Arrays Collections, Lists and Values.. oh my!
+
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "45"];
+
+muppetNames[1] = "OTTO";
+
+console.log(muppetNames[0]); //lets see the variables in the array list
+
+
+//End of Array list'''
