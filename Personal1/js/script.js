5ddcb11f57d1061000e5e5b6d6c19a2e8125955c
MesiterWebzr <meister@fullsail.edu>
2014-01-16 13:59:19 -0800

    testing finals
    final revised personal expression- moving onto industrial expression

--- Personal1/js/script.js
+++ Personal1/js/script.js
@@ -10,17 +10,19 @@
 var height = prompt("Enter your height in feet");//declaring the height value
 var inches = prompt("Enter your remaining height in inches");//declaring the inches value
 var mass = prompt("Enter your weight");//decelaring the mass (lbs) value
-var totalHeight = height * 12 + inches;//setting
-var bmiPercentage = mass / totalHeight;
-var results = bmiPercentage;
+var length = 12;//declaring the value of length for inches to be added
+var totalHeight = height * length + inches;//assigning level one calculation to declare total height
+var bmiPercentage = mass / totalHeight;//assigning level 2 calculation to set Quetelet Index or BMI
+var results = Math.round(bmiPercentage * 100);//calculting 
+var details = new Array( height + " " + "inches tall");
 
+/*alert("Your Quetelet Index is" + " " + results + "%" );//alerting user of results per  details inputed */
 
-alert("Your Quetelet Index is" + " " + results + "%" );
+alert("Based on your input of" +" " + height + "ft" + " " + inches +"in" + " " + "with body mass of" + " " + mass + "lbs" + " " + "Your Quetelet Index is" + " " + results + "%");//alerted details of user input
 
-
-console.log(height);
-console.log(inches);
-console.log(mass);
-console.log(totalHeight);
-console.log(bmiPercentage);
-console.log(results);
+console.log(height); //logged height into console
+console.log(inches);//logged inches into console
+console.log(mass);//logged mass into console
+console.log(totalHeight);//logged totalHeight into console
+console.log(bmiPercentage);//logged bmiPercentage into console
+console.log(results);//logged results into console
