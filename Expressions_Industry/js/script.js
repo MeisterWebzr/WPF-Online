00a206d663ac67d05628f5814e2a0213d2e0dc68
MesiterWebzr <meister@fullsail.edu>
2014-01-16 15:33:10 -0800

    commenting
    commenting on values set for images and pages and package details

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,14 +7,19 @@
 
 
 var pages = prompt("Enter the amount of html pages your website needs");//declare variable for html pages
-var images = prompt("Enter the amount of images you need uploaded or edited");
-var links = prompt("How many links do you need?");
-var pricePages = pages * 150.00;
-var priceImages = images * 40.00;
+var images = prompt("Enter the amount of images you need uploaded or edited");//declaring images to be added or edited on website
+var links = prompt("How many links do you need?");//declaring variable to links to be added to website
+var pricePages = pages * 150.00;//assigning value of price per html page
+var priceImages = images * 40.00;//assigning value to price per images
 var priceLinks = links * 5.00;
 var estimateTotal = priceLinks + priceImages + pricePages;
+var packageDetails = prompt("Your package includes" + " " + pages + "html pages" + " " + images + " " + "images" + " " + links + "links")//assigned concatenate string of variables for package details
 
-console.log(pages)
+
+
+
+console.log(packageDetails)
+console.log(estimateTotal);
 
 
 
