d0fb3a0cae823f447610add248711370941b02b6
MesiterWebzr <meister@fullsail.edu>
2014-01-16 15:36:33 -0800

    Estimate
    assigned  the estimator  for total package details.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,7 +12,7 @@ var links = prompt("How many links do you need?");//declaring variable to links 
 var pricePages = pages * 150.00;//assigning value of price per html page
 var priceImages = images * 40.00;//assigning value to price per images
 var priceLinks = links * 5.00;//decelared value for price per links
-var estimateTotal = priceLinks + priceImages + pricePages;
+var estimateTotal = priceLinks + priceImages + pricePages;//assigned the estimator calculations for total package details
 var packageDetails = prompt("Your package includes" + " " + pages + "html pages" + " " + images + " " + "images" + " " + links + "links")//assigned concatenate string of variables for package details
 
 
