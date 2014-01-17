e5da7593675e8e624995f26310523a873c831ec8
MesiterWebzr <meister@fullsail.edu>
2014-01-16 18:20:48 -0800

    Formula @ Parameters
    setting up formula and parameters for calculations

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,30 @@
 //Otto Burroughs 1/16/2014 Expressions_Personal
+
+
+//Expressions_Wacky
+
+//This calculator will help you calculate a loan for 
+
+//The Formula: c = ((p * r) * Math.pow((1 + r), n)) / ( Math.pow((1+4), n) - 1)
+//@param p float Amount borrowed
+//@param r interesst, as a percentage
+function calculateMortgage( ){
+
+	var monthlyPayment = null;
+
+
+	//converting this percentage to a decimal
+	r + percentToDecimal(r);
+	
+	return monthlyPayment;
+ 
+
+}
+ 
+
+ function percentToDecimal(percent){
+
+
+
+
+ }
