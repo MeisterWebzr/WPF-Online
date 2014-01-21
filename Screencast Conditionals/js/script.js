b3a739241e08edf48f342ab3855387e9f6497e1f
MesiterWebzr <meister@fullsail.edu>
2014-01-21 12:05:28 -0800

    Conditional Logic Video
    if(true){
    print to log(“message to user”);
    }

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -17,6 +17,24 @@
 
 
 
+//Conditinoal logic
+
+/*
+var oldEnough = false;
+
+//If the child is old enough, print to console, "you can ride"
+
+
+
+if(oldEnough){
+
+	//code performed if condition is true
+	console.log("You can ride the coaster");
+}
+
+console.log("You cannot ride the coaster");
+
+*/
 
 
 
