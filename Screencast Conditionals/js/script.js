edb5d8b29e3d4e638ca82c8e6d4dce6116e2d32a
MesiterWebzr <meister@fullsail.edu>
2014-01-21 13:31:12 -0800

    logical operators
    always you strict equality

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -1,7 +1,7 @@
-//Conditionals video readings
+////////////////////Conditionals video readings////////////////////
 
 
-//Conditional logic concepts
+////////////////////Conditional logic concepts////////////////////
 
 /*
 	if (sunny) {
@@ -17,7 +17,7 @@
 
 
 
-//Conditinoal logic
+////////////////////Conditinoal logic////////////////////
 
 /*
 var oldEnough = false;
@@ -36,7 +36,7 @@ console.log("You cannot ride the coaster");
 
 */
 
-//Relational Expression
+////////////////////Relational Expression////////////////////
 
 
 
@@ -47,13 +47,13 @@ var parentHeight = 45;
 
 	//If the kid height is over 48 inches
 	if (kidHeight > minHeight){
-
-		//code to be perfomed if condition is true              //make sure your order is correct for code to progress each code 1st/ 2nd/ 3rd
-		console.log("you can ride the coaster");
+														////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//code to be perfomed if condition is true      ////////////////////make sure your order is correct for code to progress each code 1st/ 2nd/ 3rd////////////////////
+		console.log("you can ride the coaster");		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }else if(kidHeight < parentHeight){
 		//code to be performed if value is true
-		console.log("You can ride with your guardian angel");
+		console.log("You can ride with your guardian angel, and they have to drive");
 
 //else if chain choose between three or more codes
 }else{
@@ -65,11 +65,74 @@ var parentHeight = 45;
 
 
 
+////////////////////Relational Operators////////////////////
+
+
+
+//greater than >
+//less than    <
+//greater than or equal to >=
+//less than or equal to <=
+
+
+
+//The purpose of these operatators are to compare "ONLY TWO" variables
+//They compare results in TRUE or FALSE
+
+
+////////////////////EQUALITY OPERATORS////////////////////
+
+
+//EQUALITY ==      
+
+//STRICT EQUALITY ===
+
+//INEQUALITY !=
+
+
+
+////////////////////////////////////////////////////////////EXAMPLE////////////////////////////////////////////////////////////
+
+var a = 12
+a > 20 //false
+
+
+///////////////////////////////////////////NO SPACES IN BETWEEN OPERATORS//////////////////////////////////////////////////////
+
+
+5 < 5 //FALSE
+
+a > b  OR b < a  ////////////////////////////////////////STATEMENT THAT ARE EQUALLY THE SAME///////////////////////////////////
+
+
+////////////////////EQUALITY////////////////////
+
+//uses the operator == operator. (No space!)
+
+
+//"the same as"
+
+
+//a == b   //as is the same value as b and b is the same value as a //compares the values of both
+
+//a = b // changes the value of a
 
 
 
 
+////////////////////STRICT  EQUALITY  ALWAYS USE STRICT EQUALITY///////
 
+
+6 == "6"  //TRUE
+6 === "6" //FALSE
+
+
+
+
+////////////////////INEQUALITY////////////////////
+
+//uses the != operator.
+//"is not the same as"
 		
 
 
