477e876d93bcd54d6e449e6b7e70fd1a4683c6a5
MesiterWebzr <meister@fullsail.edu>
2014-01-21 12:23:39 -0800

    Condition w/ relational expression
    simplify using if(){
    
    }else{

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -41,18 +41,25 @@ console.log("You cannot ride the coaster");
 
 
 
-var kidHeight = 50;
-var minHeight = 47;
+var kidHeight = 52;
+var minHeight = 48;
 
 	//If the kid height is over 48 inches
-	if (kidHeight > 48) {
+	if (kidHeight > minHeight){
 
 		//code to be perfomed if condition is true
 		console.log("you can ride the coaster");
+}else{
+		//code to be performed if valur is true
+		console.log("You dont meet the minimum height requirements to ride this ride");
 
-	};
+}
 
-console.log("You dont meet the minimum height requirements to ride this ride");
+
+
+
+
+		
 
 
 
