939c9aebd2a22a9a0327fe7283015b2fe9dbbd7f
MesiterWebzr <meister@fullsail.edu>
2014-01-22 14:31:54 -0800

    Directory setup
    settings vars and testing checking for null function

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,21 @@
+//Otto Burroughs 1-22-2014 Conditionals Industry
+
+
+
+//Calculate your weekly or monthy  pay and over time pay if applies.
+
+
+
+var rateOfPay = Number(prompt("Enter your hourly rate of pay", " ")); //Prompting use tot enter rate of pay variable
+rateOfPay = parseInt(rateOfPay); //converting number to interger for calculation
+var workHours = Number(prompt("Enter the nuber of hours you work last pay period")); //Prompting for hours to be calculated
+if (rateOfPay === " " || 0); //Checking for null or 0 dollars for rate of pay
+
+	{
+		
+		alert("Please enter a valid amount for your hourly pay!");//Notification to enter a vaild number
+		
+	}
+
+
+
