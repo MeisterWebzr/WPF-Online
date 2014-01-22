7354b17f0a5e7f3362b2184184b984a8118312ee
MesiterWebzr <meister@fullsail.edu>
2014-01-22 15:25:48 -0800

    null checker
    changing setting for null checker

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,20 +2,26 @@
 
 
 
-//Calculate your weekly or monthy  pay and over time pay if applies.
+//Calculate your weekly or monthy pay.
+//The Formula: workHours * rateOfPay =
 
 
 
 var rateOfPay = Number(prompt("Enter your hourly rate of pay", " ")); //Prompting use tot enter rate of pay variable
-rateOfPay = parseInt(rateOfPay); //converting number to interger for calculation
-var workHours = Number(prompt("Enter the nuber of hours you work last pay period")); //Prompting for hours to be calculated
-if (rateOfPay === " " || 0); //Checking for null or 0 dollars for rate of pay
+var workHours = Number(prompt("Enter the nuber of hours you work last pay period", " " )); //Prompting for hours to be calculated
+var payPeriod = Number(prompt("Enter the amount of days you worked", " "))//Prompting user for pay period term
 
-	{
+	
+	if (rateOfPay === " " || rateOfPay === null); //Checking for null or 0 dollars for rate of pay
 		
-		alert("Please enter a valid amount for your hourly pay!");//Notification to enter a vaild number
-		
-	}
+		{
+		alert("Please enter your Hourly rate \in Dollars");
+		console.log(rateOfPay, workHours, payPeriod); //printing results of variable entered to database
+		}
+
+
+
+
 
 
 
