c2972fd980255841430d2e1059e54e6edc2b94e2
MesiterWebzr <meister@fullsail.edu>
2014-01-23 14:11:45 -0800

    Committed comments
    revised code for flow and finished. Its not 20 commits but you can see
    that I’m getting better at writing the code faster. I like this course
    and wouldn’t mine coding for a living. its fun debugging now onto the
    wacky conditional

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -17,7 +17,7 @@ var workHours, amountOfProjects, timePerProject;
 	}
 
     //Validating the amount of work hours user can work daily
-    while(! (workHours > 0 ) ){
+    while (! (workHours > 0 ) ){
 
     	//2nd user prompt to enter amount of hours that user can work in a day
     	workHours = prompt("Enter the amount of hours you can work today", " ");
@@ -36,17 +36,18 @@ var workHours, amountOfProjects, timePerProject;
 
     	//This line we will assign the above calcualtion to a conditional operator
     	if (amountOfProjects >= 1 && amountOfProjects <= 4)
-    	{
+    	
+    	{	//alerting user that falls within the conditional statement of average work for a designer
             alert("Thats an average work load for a Web Designer & Developer")
-    	}
+    	}//Else if comaparison for amount of project  between 5 and 9
     	else if ( (amountOfProjects >= 5 && amountOfProjects <= 9) || (amountOfProjects >= 10 && amountOfProjects <= 19) )
-    	{	
+    	{	//alert message to user asking question about the workload
     		alert("WoW! how do you work on so many projects by yourself? Hire some students! from Full Sail");
     	}
-    	else
-    	{
+    	else //Else statement for amount of project  between 5 and 9
+    	{	//Alert message for the insane condition statment of projects
     		alert("Thats an INSANE number of projects!");
     	}
 
-
+//printing to console results of time per project that was calculated
  console.log(timePerProject);
