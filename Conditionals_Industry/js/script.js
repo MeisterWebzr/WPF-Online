e64954c5dd07249fdcc038af539f7e1045c77ffe
MesiterWebzr <meister@fullsail.edu>
2014-01-23 10:44:33 -0800

    TESTING
    debugging conditional operators for variations of variables

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -18,6 +18,7 @@ var rateOfPay, workHours, payCheck;
 	//validation of rateOfPay with the while function
 	while ( !(workHours > 0) ){
 
+        //
 		workHours = prompt("ENTER HOW MANY HOURS YOU WORKED", " ");
 		console.log(rateOfPay);	
 	}	
@@ -26,25 +27,25 @@ var rateOfPay, workHours, payCheck;
 
 	payCheck = (rateOfPay) * (workHours); //Calculating the results using expressions
 
-
+		//Alerting the user of the calculations based on their input in to the prompts
 		alert("Based on your hourly wage of $" + rateOfPay + " " + " & " + workHours + " hours of work" + " " + "you made" + " " + "$" + payCheck);
-
-		if( payCheck <= 1500)
 		
-			{
-		 		alert("thats it! You made" + " " + "$" + payCheck + " " + "How can you live on that?");
+		//assigning the calculation to a resulting variable
+		if (payCheck < 1500)
+		
+			{  //Alerting the user of the calculations based on their input from prompts
+		 	   alert("Thats it! You made" + " " + "$" + payCheck + " " + "How can you live on that?");
 
 			}
-
-		   if ( payCheck <= 2500)
-			
-			{
-			alert("Step your game up! You only made" + " " + "$" + payCheck + " " + "could be making triple that with a degree from Full Sail");
-			}
-
-		  else
-		   {
-		   	  	alert("WOW! You made" + " " + "$" + payCheck);
+			//If conditional operator for below $2500
+		   if ( payCheck >= 1501 && payCheck < 2500) 
+			 
+			{  //Alerting user on stepping their game up based on the amount made being below or eual to $2500
+			   alert("Step your game up! You only made" + " " + "$" + payCheck + " " + " You can quadruple that with a degree from Full Sail");
+			   }
+		      else
+		       {
+		   	   alert("WOW! You made" + " " + "$" + payCheck);
 		   }
 		   
 
