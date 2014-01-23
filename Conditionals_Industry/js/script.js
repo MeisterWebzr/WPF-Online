4872aa9b24a990168dc6db2e5b3cd76c998d28cb
MesiterWebzr <meister@fullsail.edu>
2014-01-22 16:27:25 -0800

    I trying to make sure the calculations are setup
    correctly and alerting correct info to user based on if then and
    comparison statements

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,28 +1,48 @@
-//Otto Burroughs 1-22-2014 Conditionals Industry
+
 
 
 
 //Calculate your weekly or monthy pay.
-//The Formula: workHours * rateOfPay =
-
+//The Formula: workHours * rateOfPay = payCheck
 
 
 var rateOfPay, workHours, payCheck; 
 
-while(!(rateOfPay > 0) ){
+	if ( !(rateOfPay > 0) ){
 
- 	rateOfPay = Number(prompt("ENTER YOUR HOURLY PAY", " ")); //Prompting use tot enter rate of pay variable
+ 		rateOfPay = Number(prompt("ENTER YOUR HOURLY PAY", " ")); //Prompting use tot enter rate of pay variable
 
-}
+    }
 
-while(!(workHours > 0) ){
+	if ( !(workHours > 0) ){
 
 	rateOfPay = Number(prompt("ENTER HOW MANY HOURS YOU WORKED", " "));
+	
+	}	
 		
+
+
+	payCheck = (rateOfPay) * (workHours);
+
+
+		alert("Based on your hourly wage of" + rateOfPay + " " + " & " + workHours + "of work" + " " + "you made" + " " + payCheck);
+
+		if( payCheck >= 6000)
 		
+			{
+		 		alert("WOW, you get paid" + payCheck + " " + " Click the ok button see your stats");
+			}
+
+		if( payCheck <= 5999 || payCheck >= 2999)
+			{
+
+				alert(" ")
+			}
 
 
-
+console.log(payCheck);
+console.log(rateOfPay, workHours);
+alert(payCheck);
 
 
 
