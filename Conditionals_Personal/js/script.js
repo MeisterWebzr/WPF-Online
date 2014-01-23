ccfa9178c9f19eaa4b11e3d38ac8787ded90f951
MesiterWebzr <meister@fullsail.edu>
2014-01-23 11:19:11 -0800

    Established
    2 prompts and validation for prompt inputs, commuted on both prompts
    also

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,4 +7,21 @@
 
 var workHours, amountOfProjects, timePerProject;
 
- 
+
+	//Validating the amount of projects with the (while) function
+	while (! (amountOfProjects > 0) ){
+		
+		//1st user prompt to enter amount of projects that need to be worked on
+		amountOfProjects = prompt("Enter the amount of projects you need to work on today", " ");
+	    console.log(amountOfProjects);
+	}
+
+    //Validating the amount of work hours user can work daily
+    while(! (workHours > 0 ) ){
+
+    	//2nd user prompt to enter amount of hours that user can work in a day
+    	workHours = prompt("Enter the amount of hours you can work today", " ");
+    	console.log(workHours);
+    }
+
+    
