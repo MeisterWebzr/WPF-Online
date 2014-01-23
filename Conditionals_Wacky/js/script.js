9ca600a12bb97fc17c944bf6e5a27193256bf19f
MesiterWebzr <meister@fullsail.edu>
2014-01-23 14:48:38 -0800

    Calculator Definition
    explained what this wacky calculator is all about

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,9 +3,18 @@
          Conditional Wacky //name of assignment
 				1-23-2014 //date
 */
-//Calculate the volume of your head
+
+/*
+ Calculate the volume of your head. I know your wondering how but 
+ being that your reading this, most likely, your a geek and geeks are
+ squares. Didn't you know that! So input your information and see 
+ how much of geek you really are. 
+
+*/
 //Formula: length * width * height = volumeofHead
 
 
+// setting the givem variables
 var length, width, height, volumeofHead;
 
+	//1st prompt
