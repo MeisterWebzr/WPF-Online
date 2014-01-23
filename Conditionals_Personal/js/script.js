9264842e671663875f71ccd2c74133ebb16be0e9
MesiterWebzr <meister@fullsail.edu>
2014-01-23 11:10:48 -0800

    Setting up Given
    determined formula and calculation

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,10 @@
+//Otto "Meister" Burroughs 1-22-2014 Conditional Personal
+
+
+//Calculate your hours of work on projects per day
+//Formula: workhours / amount of projects = timePerProject
+
+
+var workHours, amountOfProjects, timePerProject;
+
+ 
