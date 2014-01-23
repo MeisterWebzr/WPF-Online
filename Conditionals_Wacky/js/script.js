0658763a89aee31fe5e5aa9df4a1cc360e73a877
MesiterWebzr <meister@fullsail.edu>
2014-01-23 14:41:05 -0800

    set calculator
    Volume of you head.

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,11 @@
+/*
+       Otto "Meister" Burroughs //Name of student
+         Conditional Wacky //name of assignment
+				1-23-2014 //date
+*/
+//Calculate the volume of your head
+//Formula: length * width * height = volumeofHead
+
+
+var length, width, height, volumeofHead;
+
