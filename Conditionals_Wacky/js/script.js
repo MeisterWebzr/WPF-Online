433c6e0678f6b75bdcb19b2b19b2353118a44672
MesiterWebzr <meister@fullsail.edu>
2014-01-23 17:37:02 -0800

    finalizing the code
    debugging code for first alert and cleaning up all code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,24 +5,24 @@
 */
 
 /*
- Calculate the volume of your head. I know your wondering how but 
- being that your reading this, most likely, your a geek and geeks are
- squares. Didn't you know that! So input your information and see 
+ Calculate the Geekazoidness. I know your wondering how but, 
+ if your reading this, most likely. Here's where we put it to the test. 
+ Whats your Geekazoid rating! Simple input your information and see 
  how much of geek you really are. 
 
 */
 //Formula: length * width * height = volumeofHead
 
-
+	 alert( "Calculate your Geekazoidness. I know your wondering how but, if your reading this, most likely you're a geek. Here's where we put it to the test by calculating your Geekazoid rating! Simple input your information and see how much of geek you really are. Click OK to continue!" );
 
 // setting the given variables
-var length, width, height, volumeofHead;
+var length, width, height, volumeOfHead;
 
 	//validating the input of users length
 	while (! (length > 0 ) ){
 
 		//1st prompt for user to input their length of head
-		length = prompt("Enter the length of your head", " ");
+		length = prompt("Enter the amount of days you've been associated with Full Sail University", " ");
 		console.log(length);
 
 	}
@@ -30,23 +30,29 @@ var length, width, height, volumeofHead;
 	while (! (width > 0 ) ){
 
 		//1st prompt for user to input their length of head
-		width = prompt("Enter the width of your head", " ");
+		width = prompt("How many all nighters have you pulled?", " ");
 		console.log(width);
 
 	}
 	//validating the input of height of your head
 	while (! (height > 0) ) {
 
-		height = prompt("Enter the height of your head");
+		height = prompt("How many times has somewone called you a geek?", " ");
 		console.log(height);
 	}
 
 	//Calculating the results of user inputs from above prompts
-	volumeofHead = (length) * (width) * (height);
-	console.log(volumeofHead);
+	volumeOfHead = (length) * (width) * (height);
+	console.log(volumeOfHead);
 
-	alert("Congrats your head is" + " " + volumeofHead + " " + "geekazoids");
+	if (volumeOfHead >=1 && volumeOfHead <= 240000)  {
 
+		alert("Your rating is" + " " + volumeOfHead + " " + "geekazoids, I'm light years ahead of you! Bow down to the Geekazoid Master");
+	}
+	else		
+	{
+		alert(" Wow, I thought I was a Geekazoid Master but wth your rating of" + " " + volumeOfHead + " " + "geekazoids it's clear that your the new Geekazoid Master congradulations!");
+	}
 
 
 
