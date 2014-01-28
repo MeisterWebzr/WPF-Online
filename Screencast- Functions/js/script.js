f816402ca1ed1b1ccd91345aa40cf83673f4ce23
MesiterWebzr <meister@fullsail.edu>
2014-01-28 11:15:52 -0800

    Setup Directory
    added notes from first screen cast | Intro to Functions

---
+++ Screencast- Functions/js/script.js
@@ -0,0 +1,9 @@
+/*
+
+Otto Burroughs | WDDBS Student - WPF sec01 | 1/28/2014
+Screecast- Functions Follow along commit notes
+
+*/
+
+
+
---
+++ Screencast- Functions/script.js
@@ -0,0 +1,49 @@
+/*
+
+Otto Burroughs | WDDBS Student - WPF sec01 | 1/28/2014
+Screecast- Functions Follow along commit notes
+
+*/
+
+
+
+/* Functions | What a function's function? */
+
+
+
+/* Sequential - Exepresions */
+
+
+
+
+/* Conditional - If statements, ternaries */
+
+
+/* Repetitive */
+
+
+//////////////////////////* Reusable *//////////////////////////
+
+/* DRY - Don't Repeat yourself */
+
+/* Referencing to another library of code that you might use many times. You can also make changes in one place then all edits
+will be made over linked locations. Functions help organizations | BLOCKS OF CODE THAT RUN PROCESSES FOR YOU */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
