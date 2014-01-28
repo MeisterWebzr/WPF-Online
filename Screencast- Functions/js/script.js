d006fe9abab2b1118799f99b08a2e0be42cb0321
MesiterWebzr <meister@fullsail.edu>
2014-01-28 11:56:47 -0800

    cleaned directory
    ordered files correctly for testing of code and completed screencast of
    function definitions and calling functions

--- Screencast- Functions/js/script.js
+++ Screencast- Functions/js/script.js
@@ -7,3 +7,77 @@ Screecast- Functions Follow along commit notes
 
 
 
+/* Functions | What a function's function? */
+
+
+
+/* Sequential - Exepresions */
+
+
+
+
+/* Conditional - If statements, ternaries */
+
+
+/* Repetitive */
+
+
+//////////////////////////* Reusable *//////////////////////////
+
+/* DRY - Don't Repeat yourself */
+
+/* 
+
+Referencing to another library of code that you might use many times. You can also make changes in one place then all edits
+will be made over linked locations. Functions help organizations | BLOCKS OF CODE THAT RUN PROCESSES FOR YOU
+
+*/
+
+/* 
+
+     Different types of functions 
+
+		1. Functions - a.k.a. "Normal Functions"
+		2. Procedures
+		3. Anonymous Functions
+
+*/
+
+
+/*
+function functionName(){
+
+	//code the function runs
+
+}
+*/
+
+/*
+function outptMsg(){
+	//print message to world
+	console.log("Hello World, this is functions in javascript");
+}
+*/
+
+
+function calcArea(){
+
+	//function definition
+	var width = 30;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+} 
+
+
+
+calcArea(); //Invocation of function | Functions call
+
+
+
+
+
+
+
+
+
