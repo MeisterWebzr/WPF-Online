09cc8b082ccee9a0e6ed540e0422d4031f79ca98
MesiterWebzr <meister@fullsail.edu>
2014-01-29 15:52:13 -0800

    Screencast loops review
    directory setup for first Function_Industry calculator

---
+++ Functions_Industrial/js/script.js
@@ -0,0 +1,9 @@
+/*
+
+Otto Burroughs
+Screencast | Loops - Review 
+1/29/2014
+
+*/
+
+
---
+++ Screencast_Loops/js/script.js
@@ -0,0 +1,59 @@
+/*
+
+
+
+Otto Burroughs
+Screencast | Loops - Review 
+1/29/2014
+
+*///////////////////////////////LOOPS//////////////////////////////
+
+/*
+
+	var b = prompt("How many shots can you take in 5 min?", " ");//sets up the index
+
+		
+		while ( b < 10) //if statement or while statment for loop
+
+		console.log( b + ' people drunk on the floor');
+		b ++;
+		
+	}
+
+*/
+//////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////// do while loop//////////////////////////////
+
+
+/*
+
+		var c = prompt("How many times have you hooked up this year?", " ")
+
+			do{ //do setup
+				console.log(c + " kegs on the wall"); // will be run before checking if condition is true
+				c --; // incrememting or decrememting c value
+			}
+
+
+		while( c > 0); while loop after print log has run of value to check if post
+
+
+
+*/
+
+
+
+
+//////////////////////shorthand version /////////FOR LOOPS//////////// all on one line//////////////////
+
+
+
+
+for (var i = 10; i > 0; i--){
+
+	console.log(i + " Look at me now!")
+}
+
