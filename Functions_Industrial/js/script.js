8bf86d422c883b00e8ca7d1ad17b5f867dbf9228
MesiterWebzr <meister@fullsail.edu>
2014-01-29 16:48:43 -0800

    reformated
    calculator for number of gallons needed for household.

--- Functions_Industrial/js/script.js
+++ Functions_Industrial/js/script.js
@@ -1,15 +1,9 @@
 /*
 
-Otto Burroughs
-Functions| Industrial 
-1/29/2014
+Otto "Meister" Burroughs //Name of Student
+Functions Industrial //Name of assignment 
+1/29/2014 // Date
 
 */
-
-
-//calculate the amount of dog food to order
-
-	function myFunction()
-	{
-	alert("I am your water provider!");
-	}
+//calculate how many gallons of water needed for your household to consume the least amount requrired reccommended by doctors
+//Formula: (numberOfPeople * 16oz ) * (days) / (128) = gallonsOfWater needed
