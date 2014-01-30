93250926c8f0acb4c43f6c8d5f8b3c45ab668a6e
MesiterWebzr <meister@fullsail.edu>
2014-01-30 11:04:00 -0800

    DONE
    completed calculator with all comments and checked for errors

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -48,26 +48,36 @@ var totalProfit = calcProfit(income, expenses);
 	//Printing to console total profit results
 	console.log(totalProfit);
 
-
-
 	//if statement for condiontal operation of total profit
 	if(totalProfit < 0){
 
 		//alerting user of conditional statement for total profit less than 1 dollar
 		alert("Your negative" + " " + "$" + totalProfit + " " + "Have a look at your marketing plan and review your expenses for ways to cut overhead. You won't make it too much longer without making a profit." )
 		console.log(totalProfit);
+	
 	}//if statement for condiontal operation of total profit
 	if (totalProfit >= 1 && totalProfit <= 1000) {
+		
 		//alert for user to be notified of profit between 1 and 1000 dollars
 		alert("Your profited" + "$" + totalProfit + " " + "not bad, but, you an do better than that. Try charging a bit more or cutting some overhead costs.")
-	};
+		//printing to console the total
+		console.log(totalProfit)
+	}
+	
 	//if statement for condiontal operation of total profit
 	if (expenses > income) {
+		
 		//alert for user to be notified of profit between 1 and 1000 dollars
 		alert("Your expenses were" + " " + "$" + expenses + " " + "I suggest you eliminte your tedious expenses and cut overhead so you can turn a profit.")
+		//printing to console the total
+		console.log(expenses)
+	
+	//Else conditoinal statement for all other conditions not set above
 	}else{
 
 		//alert user for all ther conditions with this else conditionals statement
 		alert("You profited" + " " + income + " " );
+		//printing to console the total
+		console.log(income)
 	}
 
