c1680f8d1045fe205c0e843b127d145d234c5da3
MesiterWebzr <meister@fullsail.edu>
2014-01-30 11:42:19 -0800

    Defition of wacky functional
    wrote the definition of this calculator

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,13 @@
+/*
+
+		Otto "Meister" Burroughs //Name of Student
+		Functions Wacky //Name of assignment 
+		1/30/2014 11:30 am // Date and time finished
+
+*/
+
+/*
+		Calculate your DreamVision. This is the ability to see your dreams and create new ones.
+*/
+
+//Formula: times you sleep per day * 
